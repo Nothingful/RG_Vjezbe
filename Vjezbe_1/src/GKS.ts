18cc5558c5ce6d0e909e44756ad5d3e6acdd9f55
Antonio <antoniomax98@gmail.com>
2021-10-08 17:49:05 +0200

    Izdvojio GKS klasu u zasebnu datoteku

--- Vjezbe_1/src/GKS.ts
+++ Vjezbe_1/src/GKS.ts
@@ -9,7 +9,7 @@
  * @param {number} ymin 
  * @param {number} ymax 
  */
-class GKS {
+ class GKS {
     /** HTML canvas element */
     private canvas: HTMLCanvasElement;
     /** HTML canvas rendering context */
@@ -102,42 +102,3 @@ class GKS {
         return this.SY * Y + this.PY;
     }
 }
-
-function zadatak_2(): void{
-    var canvas = document.querySelector<HTMLCanvasElement>("#canvas");
- 
-    if (!canvas){
-       alert("No canvas found!");
-    }
-
-    const X_MIN = -5;
-    const X_MAX = 5;
-    const Y_MIN = X_MIN;
-    const Y_MAX = X_MAX;
-
-    var gks = new GKS(canvas, X_MIN, X_MAX, Y_MIN, Y_MAX);
-
-    // DRAW X AXIS LINE
-    gks.moveTo(0, 5);
-    gks.lineTo(0, -5);
-    gks.stroke();
-
-    // DRAW Y AXIS LINE
-    gks.moveTo(5, 0);
-    gks.lineTo(-5, 0);
-    gks.stroke();
-
-    // DRAW PARABOLA
-    const MOVE = 0.1;
-    gks.strokeStyle("red");
-    gks.moveTo(-5, (0.5 * -5 * -5) - 3);
-    for (let X = -5; X < 5; X += MOVE) {
-        let Y = ((0.5 * X * X) - 3);
-        gks.lineTo(X,Y);
-    }
-    gks.stroke();
- }
- 
- document.addEventListener('DOMContentLoaded', function () {
-    zadatak_2();
- });
