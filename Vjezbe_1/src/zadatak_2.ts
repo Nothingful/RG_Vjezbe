3d5ab19402dfc3baa02ed9b7afa42e759fb60397
Antonio <antoniomax98@gmail.com>
2021-10-08 17:12:14 +0200

    Zadatak 2 done. Created GKS class

--- Vjezbe_1/src/zadatak_2.ts
+++ Vjezbe_1/src/zadatak_2.ts
@@ -1,3 +1,14 @@
+/**
+ * Globalni Koordinatni Sustav
+ * 
+ * Klasa za iscrtavanje na HTML canvasu i
+ * konvertiranje matematickih X i Y koordinata na koordinate ekrana
+ * @param {HTMLCanvasElement} canvas 
+ * @param {number} xmin 
+ * @param {number} xmax 
+ * @param {number} ymin 
+ * @param {number} ymax 
+ */
 class GKS {
     /** HTML canvas element */
     private canvas: HTMLCanvasElement;
@@ -19,7 +30,7 @@ class GKS {
         this.xmin = xmin;
         this.ymax = ymax;
         this.ymin = ymin;
-        this.context = canvas.getContext("2d");
+        this.context = this.canvas.getContext("2d");
 
         var height: number = this.canvas.height;
         var width: number = this.canvas.width;
@@ -30,13 +41,64 @@ class GKS {
         this.PY = -this.SY * this.ymax;
     }
 
+    /**
+     * Postavlja "olovku" na poziciju (x, y) u globalnim koordinatama
+     * @param {number} X 
+     * @param {number} Y 
+     */
+    public moveTo(X: number, Y: number): void {
+        this.context.beginPath();
+        this.context.moveTo(this.scaleX(X), this.scaleY(Y));
+    }
+
+    /**
+     * Povlači liniju od posljednje zapamćene pozicije do (x, y) u globalnim koordinatama
+     * @param {number} X 
+     * @param {number} Y 
+     */
+    public lineTo(X: number, Y: number): void {
+        this.context.lineTo(this.scaleX(X), this.scaleY(Y));
+    }
+
+    /**
+     * Povlači liniju pozivom HTML5-rutine stroke()
+     */
+    public stroke(): void {
+        this.context.stroke();
+    }
+
+    /**
+     * Linija se povlači bojom c (npr. "red", "green", "blue", "black")
+     * @param {string | CanvasGradient | CanvasPattern} color 
+     */
+    public strokeStyle(color: string | CanvasGradient | CanvasPattern): void {
+        this.context.strokeStyle = color;
+    }
+
+    /**
+     * Update canvas size and context. Also update scaling variables.
+     * @param canvas 
+     */
+    public updateCanvas(canvas: HTMLCanvasElement): void{
+        this.canvas = canvas;
+        this.context = this.canvas.getContext("2d");
+
+        var height = this.canvas.height;
+        var width = this.canvas.width;
+        
+        this.SX = width / (this.xmax - this.xmin);
+        this.SY = -height / (this.ymax - this.ymin);
+        this.PX = -this.SX * this.xmin;
+        this.PY = -this.SY * this.ymax;
+    }
+
     /** Return pixel value for X */
-    public scaleX(X: number){
+    private scaleX(X: number){
         return this.SX * X + this.PX;
     }
 
     /** Return pixel value for Y */
-    public scaleY(Y: number){
+    private scaleY(Y: number){
         return this.SY * Y + this.PY;
     }
 }
@@ -47,50 +109,33 @@ function zadatak_2(): void{
     if (!canvas){
        alert("No canvas found!");
     }
-    
-    var height: number = canvas.height;
-    var width: number = canvas.width;
-    var g: CanvasRenderingContext2D = canvas.getContext("2d");
 
     const X_MIN = -5;
     const X_MAX = 5;
     const Y_MIN = X_MIN;
     const Y_MAX = X_MAX;
-    
-    const SX = width / (X_MAX - X_MIN);
-    const SY = -height / (Y_MAX - Y_MIN);
-    const PX = -SX * X_MIN;
-    const PY = -SY * Y_MAX;
 
-    function scaleX(X: number){
-        return SX * X + PX;
-    }
-
-    function scaleY(Y: number){
-        return SY * Y + PY;
-    }
+    var gks = new GKS(canvas, X_MIN, X_MAX, Y_MIN, Y_MAX);
 
     // DRAW X AXIS LINE
-    g.beginPath();
-    g.moveTo(scaleX(0), scaleY(5));
-    g.lineTo(scaleX(0), scaleY(-5));
-    g.stroke();
+    gks.moveTo(0, 5);
+    gks.lineTo(0, -5);
+    gks.stroke();
 
     // DRAW Y AXIS LINE
-    g.beginPath();
-    g.moveTo(scaleX(5), scaleY(0));
-    g.lineTo(scaleX(-5), scaleY(0));
-    g.stroke();
+    gks.moveTo(5, 0);
+    gks.lineTo(-5, 0);
+    gks.stroke();
 
     // DRAW PARABOLA
     const MOVE = 0.1;
-    g.strokeStyle = "red";
-    g.beginPath();
-    for (let X = -5; X < 5; X+=MOVE) {
+    gks.strokeStyle("red");
+    gks.moveTo(-5, (0.5 * -5 * -5) - 3);
+    for (let X = -5; X < 5; X += MOVE) {
         let Y = ((0.5 * X * X) - 3);
-        g.lineTo(scaleX(X),scaleY(Y));
+        gks.lineTo(X,Y);
     }
-    g.stroke();
+    gks.stroke();
  }
  
  document.addEventListener('DOMContentLoaded', function () {
