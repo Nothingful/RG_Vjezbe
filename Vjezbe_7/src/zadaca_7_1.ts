1700e0c0bb7707160964b006995128f0dbcdd5f4
Antonio <antoniomax98@gmail.com>
2021-11-30 23:33:26 +0100

    Zadaca 7 - 90% done

--- Vjezbe_7/src/zadaca_7_1.ts
+++ Vjezbe_7/src/zadaca_7_1.ts
@@ -12,109 +12,144 @@ function zadaca_7_1() {
     let u_mTrans = gl.getUniformLocation(GPUprogram1, "u_mTrans");
     let u_boja = gl.getUniformLocation(GPUprogram1, "u_boja");
 
-    function drawElipsis(vertices: number[]){
-        gl.viewport(0, 0, canvas.width, canvas.height);
-
-        function draw(px: number, py: number, k: number) {
-            let angle = Math.PI / k;
-            gl.uniformMatrix3fv(u_mTrans, false, [Math.cos(angle), Math.sin(angle), 0.0, -Math.sin(angle), Math.cos(angle), 0.0, px, py, 1.0]);
-            gl.uniform4fv(u_boja, [1.0, 1.0, 0.0, 1.0]); // žuta boja
-            gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length / 2);
-        }
-
-        draw(0.0, 0.5, 2);
-        draw(0.0, -0.5, 2);
-        draw(0.5, 0.0, 1);
-        draw(-0.5, 0.0, 1);
-        draw(0.33, 0.38, 3.5);
-        draw(0.33, -0.38, -3.5);
-        draw(-0.33, -0.38, 3.5);
-        draw(-0.33, 0.38, -3.5);
-    }
-
-    // definiranje geometrije preko javascript polja
-    var mt2D = new MT2D();
-    //mt2D.projection2D(0, canvas.width, 0, canvas.height);
-
-    /**
-     * Cretes array of vertices for drawing elipsis
-     * @returns array of elipsis vertices
-     */
-    function generateElipsisVertices(a: number, b: number): number[] {
-        var vertices = [];
-        for (let t = 0; t <= 2 * Math.PI+1; t += 0.01) {
-            vertices.push(a * Math.cos(t));
-            vertices.push(b * Math.sin(t));
-            /*vertices.push(mt2D.transX(a * Math.cos(t)));
-            vertices.push(mt2D.transY(b * Math.sin(t)));*/
-        }
-        //console.log(vertices);
+    function cube_page(a: number, color: number[]): number[][] {
+        let vertices = [[-a,  a].concat(color),
+                        [-a, -a].concat(color),
+                        [ a, -a].concat(color),
+                        [ a,  a].concat(color),
+                        [-a,  a].concat(color),
+                        [ a, -a].concat(color)];
         return vertices;
     }
 
-    function fillBuffers(vertices: number[]) {
+    function fillBuffers(vertices: number[][]) {
         var vertexBuffer: WebGLBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
-        // povezivanje s atribut varijablom a_vrhXY u programu za sjenčanje
+        // povezivanje s atribut varijablama u programu za sjenčanje
         let a_vrhXY = gl.getAttribLocation(GPUprogram1, "a_vrhXY");
+        let a_boja = gl.getAttribLocation(GPUprogram1, "a_boja");
+        
+        var vertexBuffer: WebGLBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
         gl.enableVertexAttribArray(a_vrhXY);
-        gl.vertexAttribPointer(a_vrhXY, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(a_boja);
+        gl.vertexAttribPointer(a_vrhXY, 2, gl.FLOAT, false, 20, 0);
+        gl.vertexAttribPointer(a_boja, 3, gl.FLOAT, false, 20, 8);
         // punjenje spremnika - podaci koji se šalju na GPU
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices.flat()), gl.STATIC_DRAW);
     }
 
-    function draw(){
-        gl.clearColor(0.4, 0.4, 0.4, 1);
-        gl.clear(gl.COLOR_BUFFER_BIT);
+    /*var green_color = [0.0, 1.0, 0.0, 1.0]; // zelena boja
+    var red_color = [0.8, 0.0, 0.2, 1.0]; // crvena boja
+    var blue_color = [0.1, 0.54, 0.98, 1.0]; // plava boja*/
+    var zuto = [1, 1, 0];
+    var crveno = [1, 0, 0];
+    var ljubicasto = [0.5, 0, 1];
+    var zeleno = [0, 1, 0];
+    var plavo = [0, 0, 1];
+    var tirkizno = [0, 1, 1];
+    
+    var mt3D = new MT3D();
+    var STEP = 1;
+    var a = 0.75;
+    var cube_page_vertices = [];
+    function draw() {
+        gl.clearColor(0.5, 0.5, 0.5, 1);
+        //gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         gl.viewport(0, 0, canvas.width, canvas.height);
+        let radians = MT3D.toRad(STEP);
+        
+        // CRVENO
 
-        //mt2D.projection2D(-10, 10, -10, 10);
-        var green_color = [0.0, 1.0, 0.0, 1.0]; // zelena boja
-        var red_color = [0.8, 0.0, 0.2, 1.0]; // crvena boja
-        var blue_color = [0.1, 0.54, 0.98, 1.0]; // plava boja
+        mt3D.setIdentityMatrix();
+        mt3D.rotateAroundX(MT3D.toRad(-180));
+        mt3D.translate(0, 0, a);
+        mt3D.rotateAroundY(radians);
+        mt3D.rotateAroundX(radians*2);
+        mt3D.rotateAroundZ(radians*3);
+        
+        gl.uniformMatrix4fv(u_mTrans, false, mt3D.list());
+        cube_page_vertices = cube_page(a, crveno);
+        fillBuffers(cube_page_vertices);
+        gl.drawArrays(gl.TRIANGLES, 0, cube_page_vertices.length);
 
-        /********************* */
+        // ZUTO
 
-        mt2D.setIdentityMatrix();
-        var vertices_el1 = generateElipsisVertices(6, 3);
-        //mt2D.translate(mt2D.transX(4), mt2D.transY(0));
-        mt2D.translate(4, 0);
-        mt2D.rotate(MT2D.toRad(-30));
-        mt2D.projection2D(-10, 10, -10, 10);
-        fillBuffers(vertices_el1);
+        mt3D.setIdentityMatrix();
+        mt3D.rotateAroundY(MT3D.toRad(-90));
+        mt3D.translate(a, 0, 0);
+        mt3D.rotateAroundY(radians);
+        mt3D.rotateAroundX(radians*2);
+        mt3D.rotateAroundZ(radians*3);
+        
+        gl.uniformMatrix4fv(u_mTrans, false, mt3D.list());
+        cube_page_vertices = cube_page(a, zuto);
+        fillBuffers(cube_page_vertices);
+        gl.drawArrays(gl.TRIANGLES, 0, cube_page_vertices.length);
 
-        gl.uniformMatrix3fv(u_mTrans, false, mt2D.list());
-        gl.uniform4fv(u_boja, red_color);
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices_el1.length / 2);
+        // LJUBICASTO
 
-        /********************* */
+        mt3D.setIdentityMatrix();
+        mt3D.rotateAroundY(MT3D.toRad(90));
+        mt3D.translate(-a, 0, 0);
+        mt3D.rotateAroundY(radians);
+        mt3D.rotateAroundX(radians*2);
+        mt3D.rotateAroundZ(radians*3);
+        
+        gl.uniformMatrix4fv(u_mTrans, false, mt3D.list());
+        cube_page_vertices = cube_page(a, ljubicasto);
+        fillBuffers(cube_page_vertices);
+        gl.drawArrays(gl.TRIANGLES, 0, cube_page_vertices.length);
 
-        mt2D.setIdentityMatrix();
-        var vertices_el2 = generateElipsisVertices(6, 3);
-        mt2D.rotate(MT2D.toRad(-30));
-        mt2D.translate(4, 0);
-        mt2D.projection2D(-10, 10, -10, 10);
-        fillBuffers(vertices_el2);
+        // ZELENO
 
-        gl.uniformMatrix3fv(u_mTrans, false, mt2D.list());
-        gl.uniform4fv(u_boja, blue_color);
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices_el2.length / 2);
+        mt3D.setIdentityMatrix();
+        mt3D.translate(0, 0, -a);
+        mt3D.rotateAroundY(radians);
+        mt3D.rotateAroundX(radians*2);
+        mt3D.rotateAroundZ(radians*3);
+        
+        gl.uniformMatrix4fv(u_mTrans, false, mt3D.list());
+        cube_page_vertices = cube_page(a, zeleno);
+        fillBuffers(cube_page_vertices);
+        gl.drawArrays(gl.TRIANGLES, 0, cube_page_vertices.length);
 
-        /********************* */
+        // PLAVO
 
-        mt2D.setIdentityMatrix();
-        var vertices_el3 = generateElipsisVertices(4, 1);
-        mt2D.rotate(MT2D.toRad(75));
-        mt2D.translate(3, 0);
-        mt2D.mirrorForY();
-        mt2D.projection2D(-10, 10, -10, 10);
-        fillBuffers(vertices_el3);
+        mt3D.setIdentityMatrix();
+        mt3D.rotateAroundX(MT3D.toRad(-90));
+        mt3D.translate(0, -a, 0);
+        mt3D.rotateAroundY(radians);
+        mt3D.rotateAroundX(radians*2);
+        mt3D.rotateAroundZ(radians*3);
+        
+        gl.uniformMatrix4fv(u_mTrans, false, mt3D.list());
+        cube_page_vertices = cube_page(a, plavo);
+        fillBuffers(cube_page_vertices);
+        gl.drawArrays(gl.TRIANGLES, 0, cube_page_vertices.length);
 
-        gl.uniformMatrix3fv(u_mTrans, false, mt2D.list());
-        gl.uniform4fv(u_boja, green_color);
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices_el3.length / 2);
+        // TIRKIZNO
+
+        mt3D.setIdentityMatrix();
+        mt3D.rotateAroundX(MT3D.toRad(90));
+        mt3D.translate(0, a, 0);
+        mt3D.rotateAroundY(radians);
+        mt3D.rotateAroundX(radians*2);
+        mt3D.rotateAroundZ(radians*3);
+        
+        gl.uniformMatrix4fv(u_mTrans, false, mt3D.list());
+        cube_page_vertices = cube_page(a, tirkizno);
+        fillBuffers(cube_page_vertices);
+        gl.drawArrays(gl.TRIANGLES, 0, cube_page_vertices.length);
+        
+        if (STEP < 360) STEP += 0.15;
+        else STEP = 0;
+        requestAnimationFrame(draw);
     }
-
+    //gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    //gl.cullFace(gl.FRONT);
     draw();
 }
